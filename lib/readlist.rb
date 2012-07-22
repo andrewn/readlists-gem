8625cc11369e6cc410962efe447661af88fbaa32
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2012-07-22 15:49:43 +0100

    Store session id for future requests

--- lib/readlist.rb
+++ lib/readlist.rb
@@ -5,7 +5,7 @@ Bundler.require(:default)
 require "json"
 
 class ReadList
-  attr_reader :url
+  attr_reader :url, :session_id
 
   def self.create
     response = RestClient.post('http://readlists.com/api/v1/readlists/', {}.to_json, :content_type => :json, :accept => :json)
@@ -13,10 +13,13 @@ class ReadList
     raise "#{response.code} is not a 201 response" unless response.code == 201
     raise "No location given" unless response.headers[:location]
 
-    new({ :url => response.headers[:location] })
+    session_id = response.cookies["sessionid"]
+
+    new({ :url => response.headers[:location], :session_id => session_id })
   end
 
   def initialize(params={})
-    @url = params[:url]
+    @url        = params[:url]
+    @session_id = params[:session_id]
   end
 end
--- test/test_readlist.rb
+++ test/test_readlist.rb
@@ -3,22 +3,28 @@ require 'webmock/minitest'
 require './lib/readlist'
 
 class TestReadList < MiniTest::Unit::TestCase
-  def test_creates_anonymous_readlist
-    readlist = ReadList.new
-    refute_nil readlist
+  def setup
+    @stubbed_create = stub_request(:post, 'http://readlists.com/api/v1/readlists/')
+                                .with(
+                                  :body    => {},
+                                  :headers => {"Content-Type" => "application/json", "Accept" => "application/json"}
+                                )
+                                .to_return(
+                                  :status => 201,
+                                  :headers => {
+                                    "Location"   => "http://readlists.com/api/v1/readlists/075c62a3/",
+                                    "Set-Cookie" => "sessionid=2847c36286d39d7f10f7c0a9b6a1dd48; expires=Sun, 05-Aug-2012 14:10:48 GMT; httponly; Max-Age=1209600; Path=/"
+                                  })
   end
 
-  def test_has_url
-    stub = stub_request(:post, 'http://readlists.com/api/v1/readlists/')
-                    .with(
-                      :body    => {},
-                      :headers => {"Content-Type" => "application/json", "Accept" => "application/json"}
-                    )
-                    .to_return(:status => 201, :headers => { "Location" => "http://readlists.com/api/v1/readlists/075c62a3/"})
-
+  def test_creates_anonymous_readlist
     readlist = ReadList.create
 
-    assert_requested stub
-    assert_equal readlist.url, 'http://readlists.com/api/v1/readlists/075c62a3/'
+    assert_equal 'http://readlists.com/api/v1/readlists/075c62a3/', readlist.url
+  end
+
+  def test_stores_session_for_future_requests
+    readlist = ReadList.create
+    assert_equal "2847c36286d39d7f10f7c0a9b6a1dd48", readlist.session_id
   end
 end
